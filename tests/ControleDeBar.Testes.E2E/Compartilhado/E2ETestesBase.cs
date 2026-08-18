81f0f4f0235ff9292d6d36ff16b67f886186a17d
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-08-18 16:02:01 -0300

    Adiciona testes E2E para autenticação de usuários, incluindo login e registro

--- tests/ControleDeBar.Testes.E2E/Compartilhado/E2ETestesBase.cs
+++ tests/ControleDeBar.Testes.E2E/Compartilhado/E2ETestesBase.cs
@@ -1,6 +1,10 @@
+using ControleDeBar.Testes.E2E.Modulos.ModuloAutenticacao;
+using ControledeDeBar.Testes.E2E.Compartilhado;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Playwright.MSTest;
 
-namespace ControledeDeBar.Testes.E2E.Compartilhado;
+namespace ControleDeBar.Testes.E2E.Compartilhado;
 
 public abstract class E2ETestsBase : PageTest
 {
@@ -28,5 +32,38 @@ public abstract class E2ETestsBase : PageTest
         {
             aplicacao = null!;
         }
+    }
+
+    protected async Task RegistrarUsuarioAsync(string email, string senha)
+    {
+        using IServiceScope scope = aplicacao.Services.CreateScope();
+
+        UserManager<IdentityUser<Guid>> userManager =
+            scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser<Guid>>>();
+
+        IdentityUser<Guid> user = new IdentityUser<Guid>()
+        {
+            Id = Guid.CreateVersion7(),
+            UserName = email,
+            Email = email
+        };
+
+        IdentityResult resultado = await userManager.CreateAsync(user, senha);
+
+        Assert.IsTrue(
+            resultado.Succeeded,
+            string.Join("; ", resultado.Errors.Select(erro => erro.Description))
+        );
+    }
+
+    protected async Task RegistrarEEntrarAsync(string email, string senha)
+    {
+        await RegistrarUsuarioAsync(email, senha);
+
+        EntrarPage entrarPage = new(Page, UrlBase);
+
+        await entrarPage.IrParaAsync();
+        await entrarPage.PreencherAsync(email, senha);
+        await entrarPage.ConfirmarAsync();
     }
 }
